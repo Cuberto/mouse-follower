145a08c80635c33dc8bfa423136b52c0764af23e
Artem Draft <artemon_93@mail.ru>
2022-04-07 19:01:40 +0300

    v.1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        library: {
+            name: 'MouseFollower',
+            type: 'umd',
+            export: 'default',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+                options: {
+                    compact: true
+                },
+            },
+        ]
+    },
+    devtool: 'source-map',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
+    },
+};
